727f5adec0128ef367691fc758a2ba5e407f02d8
rubangarmon <rubangarmon99@gmail.com>
2024-02-16 15:21:03 -0600

    first commit

---
+++ SimpleWeatherApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleWeatherApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IActionResult Get()
+        {
+            try
+            {
+                var rng = new Random();
+                if (rng.Next(0, 5) < 2)
+                {
+                    throw new Exception("Oops I did it again");
+                }
+                return Ok(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+                .ToArray());
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Something went wrong");
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
